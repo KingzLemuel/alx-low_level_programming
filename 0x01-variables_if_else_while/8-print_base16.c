d7f65d6c5c1eeee07914bdfcbefc982b1d5c4652
kingzLemuel <kingzLemuel@example.com>
2023-03-17 21:55:00 +0100

    no going back

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+int n; int hex;
+for (n = '0'; n <= '9'; n++)
+{
+putchar (n);
+}
+for (hex = 'a'; hex <= 'f'; hex++)
+{
+putchar (hex); 
+}
+putchar('\n');
+return (0);
+}
