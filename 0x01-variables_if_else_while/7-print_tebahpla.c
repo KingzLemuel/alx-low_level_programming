8a382a6ee0c758857a2c79b90d03a8f05bf72a8b
kingzLemuel <kingzLemuel@example.com>
2023-03-17 17:38:09 +0100

    no going back

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 'z'; n >= 'a'; n++)
+{
+putchar (n);
+}
+putchar('\n');
+return (0);
+}
