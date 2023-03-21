7642749002185f49b4d59f26ea364b0db443ec38
kingzLemuel <olemuel64@example.com>
2023-03-21 18:20:41 +0100

    C functions at work

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet in lower case 10x
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+int j = 0;
+while (j <= 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+j++;
+}
+}
