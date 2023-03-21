d57b360cb6b8ed44f0e3a17fe6e405276436cf2f
kingzLemuel <olemuel64@example.com>
2023-03-21 18:50:24 +0100

    C functions at work

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_islower - prints alphabet in lower case 10x
+ *@c: the character to be checked
+ * Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+if ((c = 'a') && (c <= 'z'))
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
