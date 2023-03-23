756ccc4cd49218204826c0e8f561e797002c6d2e
kingzLemuel <olemuel64@example.com>
2023-03-23 20:55:26 +0100

    the law of multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *mul - give the product of two numbers
+ *
+ *@a: the first number
+ *@b: the second number
+ * Return: the product of two numbers
+*/
+int mul(int a, int b)
+{
+int mult;
+mult = a * b;
+return (mult);
+}
