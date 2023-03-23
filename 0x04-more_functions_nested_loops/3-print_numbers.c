cc82a4c9945850ee06b04f6754612ba4a808cdde
kingzLemuel <olemuel64@example.com>
2023-03-23 21:27:41 +0100

    the law of multiplication

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_numbers - print numbers 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+_putchar(a);
+}
+{
+_putchar('\n');
+}
+}
