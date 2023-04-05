8df1cbe308ea631d7cd501c1128b819181e076d0
kingzLemuel <olemuel64@example.com>
2023-04-05 17:39:33 +0100

    working with putsand recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - prints a string
+ * @s: the whose lenght is returned
+ * Return: strlen
+ *
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+else
+{
+return 1 + _strlen_recursion(s + 1);
+}
+}
