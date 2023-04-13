e49a601f6276f5b0e4a78f02cf900fdb66b312d3
kingzLemuel <olemuel64@example.com>
2023-04-13 23:14:20 +0100

    diving deeper into malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: size of allocated memory
+ *
+ * Return: The array
+*/
+void *malloc_checked(unsigned int b)
+{
+    int *reserved;
+
+    reserved = malloc(b);
+    if (reserved == NULL)
+    {
+        exit(98);
+    }
+    return (reserved);
+}
