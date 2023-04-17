abdc6fe7eb3d3270ccc41bcdb3d4b3f55ab004ab
kingzLemuel <olemuel64@example.com>
2023-04-17 16:02:23 +0100

    Going from micro to MACRO

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - Entry point
+ *
+ * Rturn: Always 0 (Success)
+ *
+*/
+int main(void)
+
+{
+printf("%s\n", __FILE__);
+return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+These programs are centeed on the principles and practices of MACROS
